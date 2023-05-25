abeb7cb237742bb186060fbc74c80ab1289939cc
Nivanka Fonseka <nivanka@silverstripers.com>
2023-05-25 21:56:35 +1200

    fixed UTC time format for NZ

--- src/Extension/DatetimeExtension.php
+++ src/Extension/DatetimeExtension.php
@@ -9,10 +9,24 @@ class DatetimeExtension extends Extension
 
     public function Cin7Date()
     {
+
+        $time = $this->owner->Value;
+        $nzst = new \DateTimeZone('Pacific/Auckland');
+        $utc = new \DateTimeZone('UTC');
+        $dateNZ = new \DateTime($time, $nzst);
+        $dateUTC = $dateNZ->setTimezone($utc);
+        $utcDate = sprintf(
+            '%sT%s+12:00',
+            $dateUTC->format('Y-m-d'),
+            $dateUTC->format('H:i:s')
+        );
+        return $utcDate;
+
+
         return sprintf(
             '%sT%s',
             $this->owner->Format('yyyy-MM-dd'),
-            $this->owner->Format('kk:mm:ssZZZZZ')
+            $this->owner->Format('HH:mm:ssZZZZZ')
         );
     }
 
