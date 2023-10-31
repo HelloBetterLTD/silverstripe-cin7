767f8d6d6fc646106f2e2dcbf25a1c951ed1eb16
Nivanka Fonseka <nivanka@silverstripers.com>
2023-10-31 13:04:54 +1300

    Tempfix
    date time conversion fixed for NZST

--- src/Extension/DatetimeExtension.php
+++ src/Extension/DatetimeExtension.php
@@ -9,25 +9,30 @@ class DatetimeExtension extends Extension
 
     public function Cin7Date()
     {
+        $dateString = $this->owner->Value;
+        $date = new \DateTime($dateString);
+        $cin7Date = $date->format('Y-m-d\TH:i:s') . '+12:00';
+        return $cin7Date;
 
-        $time = $this->owner->Value;
-        $nzst = new \DateTimeZone('Pacific/Auckland');
-        $utc = new \DateTimeZone('UTC');
-        $dateNZ = new \DateTime($time, $nzst);
-        $dateUTC = $dateNZ->setTimezone($utc);
-        $utcDate = sprintf(
-            '%sT%s+12:00',
-            $dateUTC->format('Y-m-d'),
-            $dateUTC->format('H:i:s')
-        );
-        return $utcDate;
+        
+        // $time = $this->owner->Value;
+        // $nzst = new \DateTimeZone('Pacific/Auckland');
+        // $utc = new \DateTimeZone('UTC');
+        // $dateNZ = new \DateTime($time, $nzst);
+        // $dateUTC = $dateNZ->setTimezone($utc);
+        // $utcDate = sprintf(
+        //     '%sT%s+12:00',
+        //     $dateUTC->format('Y-m-d'),
+        //     $dateUTC->format('H:i:s')
+        // );
+        // return $utcDate;
 
 
-        return sprintf(
-            '%sT%s',
-            $this->owner->Format('yyyy-MM-dd'),
-            $this->owner->Format('HH:mm:ssZZZZZ')
-        );
+        // return sprintf(
+        //     '%sT%s',
+        //     $this->owner->Format('yyyy-MM-dd'),
+        //     $this->owner->Format('HH:mm:ssZZZZZ')
+        // );
     }
 
 }
